096c50a7a3a0a3de2bff9f8deb6ca1d46c259624
Ton Prénom Nom <ton@email.com>
2026-03-29 00:06:00 +0100

    feat: add language selection screen UI (rochdi)

---
+++ app/src/main/java/com/example/konnash/LanguageActivity.kt
@@ -0,0 +1,12 @@
+package com.example.konnash
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class LanguageActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_language)
+    }
+}
